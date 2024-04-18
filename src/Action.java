f50996621b9f1469aaeeaaddbcddd98753ad2bd6
Tsoy_Kirill <tsoykirya@gmail.com>
2024-04-18 22:35:59 +0300

    first version of calculator

--- src/Action.java
+++ src/Action.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Action {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Action implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Main.current += e.getActionCommand();
+        Frame.textArea.append(e.getActionCommand());
+
+        if (e.getActionCommand().equals("=")){
+            Main.current = Main.getResult(Main.current);
+            Frame.textArea.setText(Main.current);
+        }
+        else if (e.getActionCommand().equals("c")) {
+            Main.current = "";
+            Frame.textArea.setText("");
+        }
+
+    }
 }
--- src/Frame.java
+++ src/Frame.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Frame {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Frame extends JFrame{
+        static JPanel panelOut;
+        static JTextArea textArea;
+        static JPanel panelIn;
+        Frame(String text){
+            super(text);
+
+            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            setBounds(350,  50, 800, 600);
+
+            panelOut = new JPanel();
+            panelOut.setBackground(Color.DARK_GRAY);
+            panelOut.setLayout(new FlowLayout(FlowLayout.CENTER, 70, 70));
+
+            textArea = new JTextArea();
+            textArea.setEditable(false);
+            textArea.setFont(new Font("Times New Roman", Font.BOLD, 20));
+            textArea.addKeyListener(new KeyAdapter() {
+/*
+*/
+
+                @Override
+                public void keyPressed(KeyEvent e){
+                    if (e.getKeyCode() != KeyEvent.VK_BACK_SPACE  && e.getKeyCode() != KeyEvent.VK_ENTER && e.getKeyCode() != KeyEvent.VK_SHIFT) {
+                        System.out.println(e.getExtendedKeyCode());
+                        textArea.append("" + e.getKeyChar());
+                        Main.current += e.getKeyChar();
+                    }
+                    else if (e.getKeyCode() == KeyEvent.VK_ENTER){
+                        Main.current += "=";
+                        Main.current = Main.getResult(Main.current);
+                        textArea.setText(Main.current);
+                    }
+
+                    else if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE){
+                        Main.current = Main.current.substring(0, Main.current.length() - 1);
+                        textArea.setText(Main.current);
+                    }
+                }
+            });
+
+
+
+            panelOut.add(textArea);
+            add(panelOut, BorderLayout.NORTH);
+            setVisible(true);
+
+
+            panelIn = new JPanel();
+            panelIn.setLayout(new GridLayout(5, 4, 20, 20));
+
+
+            char[] buttons = {'c', '.', '%', '√',
+                    '7', '8', '9', '/',
+                    '4', '5', '6', '*',
+                    '1', '2', '3', '-',
+                    '0', 'e', '=', '+'};
+
+            for (char c: buttons){
+                JButton button = new JButton("" + c);
+                button.setBackground(Color.pink);
+                button.setFont(new Font("Times New Roman", Font.BOLD, 20));
+                button.addActionListener(new Action());
+                panelIn.add(button);
+            }
+
+
+            add(panelIn, BorderLayout.CENTER);
+            setVisible(true);
+        }
+
+    }
+
